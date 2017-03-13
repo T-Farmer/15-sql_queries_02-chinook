e01652ed56c62ed7439fe283f3829864a929050c
T-Farmer <terrillfarmer@gmail.com>
2017-03-13 08:52:30 -0500

    a few more corrected and completed

--- chinook_queries.sql
+++ chinook_queries.sql
@@ -35,7 +35,7 @@ WHERE BillingCountry = 'Brazil';
 -- ?Provide a query that shows the invoices associated with each sales agent.
 -- ?The resultant table should include the Sales Agent's full name.
 
-SELECT Employee.FirstName, Employee.LastName, Invoice
+SELECT Employee.FirstName, Employee.LastName
 FROM Invoice
 LEFT JOIN Customer ON Customer.CustomerId = Invoice.CustomerId
 LEFT JOIN Employee ON Employee.EmployeeId = Customer.SupportRepId;
@@ -44,4 +44,45 @@ LEFT JOIN Employee ON Employee.EmployeeId = Customer.SupportRepId;
 -- Country and Sale Agent name for all invoices and customers.
 SELECT Invoice.Total, Invoice.CustomerId, Invoice.BillingCountry,
        Customer.FirstName, Customer.LastName, Customer.SupportRepId
-FROM Invoice, Customer;
+FROM   Invoice, Customer;
+LEFT JOIN Customer ON Customer.CustomerId = Invoice.CustomerId
+LEFT JOIN Employee ON Employee.EmployeeId = Customer.SupportRepId;
+
+-- How many Invoices were there in 2009 and 2011?
+-- What are the respective total sales for each of those years?
+
+SELECT COUNT(InvoiceId) AS 'Total Sales', SUM(Total)
+FROM Invoice
+WHERE substr (InvoiceDate, 1, 4) = '2009'
+OR substr (InvoiceDate, 1, 4) = '2011'
+GROUP BY substr (InvoiceDate, 1, 4);
+
+-- Looking at the InvoiceLine table, provide a query that COUNTs
+-- the number of line items for each Invoice.
+-- HINT: GROUP BY
+
+SELECT COUNT(InvoiceId)
+FROM InvoiceLine
+GROUP BY InvoiceId
+
+-- Provide a query that shows the total number of tracks in each playlist.
+-- The Playlist name should be included on the resultant table.
+
+SELECT InvoiceLine.InvoiceLineId, Track.Name
+FROM InvoiceLine, Track
+WHERE InvoiceLine.TrackId = Track.GenreId;
+
+-- Provide a query that includes the purchased track name AND artist name
+-- with each invoice line item.
+
+SELECT InvoiceLine.InvoiceLineId, Track.Name, Artist.Name
+FROM InvoiceLine
+LEFT JOIN Track ON InvoiceLine.TrackId = Track.TrackId
+LEFT JOIN Album ON Album.AlbumId = Track.AlbumId
+LEFT JOIN Artist ON Artist.ArtistId = Album.ArtistId;
+
+-- Provide a query that shows the # of invoices per country. HINT: GROUP BY
+
+SELECT COUNT(InvoiceId) AS 'Invoices Per Country', BillingCountry
+FROM Invoice
+GROUP BY BillingCountry;
