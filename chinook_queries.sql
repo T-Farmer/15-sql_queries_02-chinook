d9ff9281c6c64fef1ec339853b69376ad3b05b8d
T-Farmer <terrillfarmer@gmail.com>
2017-03-13 08:26:32 -0500

    better understanding of SQL, in the shop

--- chinook_queries.sql
+++ chinook_queries.sql
@@ -17,3 +17,31 @@ SELECT Customer.FirstName, Customer.LastName, Invoice.InvoiceId, Invoice.Invoice
 FROM   Customer, Invoice
 ON     Customer.CustomerId = Invoice.CustomerId
 WHERE  Customer.Country = 'Brazil';
+
+-- Provide a query showing only the Employees who are Sales Agents.
+SELECT Employee.FirstName, Employee.LastName, Employee.Title
+FROM Employee
+WHERE Title LIKE 'Sales% %Agent';
+
+-- Provide a query showing a unique list of billing countries from the Invoice table
+SELECT DISTINCT Invoice.BillingCountry
+FROM Invoice;
+
+-- Provide a query showing the invoices of customers who are from Brazil.
+SELECT *
+FROM Invoice
+WHERE BillingCountry = 'Brazil';
+
+-- ?Provide a query that shows the invoices associated with each sales agent.
+-- ?The resultant table should include the Sales Agent's full name.
+
+SELECT Employee.FirstName, Employee.LastName, Invoice
+FROM Invoice
+LEFT JOIN Customer ON Customer.CustomerId = Invoice.CustomerId
+LEFT JOIN Employee ON Employee.EmployeeId = Customer.SupportRepId;
+
+-- Provide a query that shows the Invoice Total, Customer name,
+-- Country and Sale Agent name for all invoices and customers.
+SELECT Invoice.Total, Invoice.CustomerId, Invoice.BillingCountry,
+       Customer.FirstName, Customer.LastName, Customer.SupportRepId
+FROM Invoice, Customer;
